823d7428626ef65852c0215b13b792a26d00d99b
nicogoolim <18001836@edu.spbgasu.ru>
2022-06-28 19:04:06 +0300

    добавил вывод до преобразования

--- MNKSTech/MNKSTech.cpp
+++ MNKSTech/MNKSTech.cpp
@@ -4,8 +4,15 @@ int main()
 {
     std::vector<int> x = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
     std::vector<int> y = { 10, 11, 11, 11, 11, 10, 11, 11, 11, 11, 10};
-    int n = 4;
+    int n = 3;
     int count = 1;
+    for (int i = 0; i < x.size(); i++)
+    {
+        if (y[i] != 0) {
+            std::cout << "(" << x[i] << ", " << y[i] << ")" << std::endl;
+        }
+    }
+    std::cout << std::endl;
     for (int  i = 0; i < x.size()-1; i++)
     {
         if (y[i]==y[i+1] && y[i]!=0 )
